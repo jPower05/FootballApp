9cb6d2d86c2f2e703250fd1436a1e2806e8bb3ba
20067779 <20067779@it103imac07.wit.ie>
2016-11-30 20:01:16 +0000

    this also doesn't work. Tried loading local json file

--- FootballApp/ViewController.swift
+++ FootballApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //loadJson (filename: "data.json")
+        loadJson()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,44 +23,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
     //Fuck this
     
-    func translate(_ sender: AnyObject) {
-        //var stringUrl = "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/{league_slug}/seasons/{season_slug}/standings"
-        //stringUrl = stringUrl.stringByReplacingOccurrencesOfString(" ", withString: "+")
-        let urlStr:String = ("https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/{league_slug}/seasons/{season_slug}/standings")
-        let url = URL(string: urlStr)
-        let session = URLSession.shared
-        let mutableRequest = NSMutableURLRequest(url: url! as URL)
-        mutableRequest.setValue("application/json", forHTTPHeaderField: "Accept")
-        mutableRequest.setValue("s1nDqzQHqmmshHlVWz1I1mBq6KrMp1tF7IfjsnnEi5X3AaUEwu", forHTTPHeaderField: "X-Mashape-Key")
-        
-        var result = "<Translation Error>"
-        
-        session.dataTask(with: url!){
-            (data, response, error) in
-            
-        if let httpResponse = response as? HTTPURLResponse {
-            if(httpResponse.statusCode == 200){
-                
-                let jsonDict: NSDictionary = try! JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
-                let dataDictionary = jsonDict["data"] as! NSDictionary
-                let standingsArray = dataDictionary["standings"] as! [String:Any]
-                for standings in standingsArray{
-                    //let position = standings["position"]! as! [String:Any]
-                    //let
+    
+    
+    func loadJson() {
+        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
+            do {
+                let jsonData = try NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe)
+                do {
+                    let jsonResult: NSDictionary = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                   
+                    if let data : [NSDictionary] = jsonResult["data"] as? [NSDictionary] {
+                        for data: NSDictionary in data {
+                            for (name,value) in data {
+                                print("\(name) , \(value)")
+                            }
+                        }
+                    }
                 }
-                
+                catch {
+                print("catch1")
+                }
             }
+            catch {
+            print("catch2")
             }
-            
-            DispatchQueue.main.async {
-                
-            }
-        }.resume()
+        }
     }
-        
-
-    
-    
 }
