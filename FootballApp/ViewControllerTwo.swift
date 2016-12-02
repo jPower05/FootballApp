134f3897f2a0c7268488e97ec137f3356122f4f5
20067779 <20067779@it103imac07.wit.ie>
2016-12-02 15:10:36 +0000

    setup ongoing

--- FootballApp/ViewControllerTwo.swift
+++ FootballApp/ViewControllerTwo.swift
@@ -14,6 +14,17 @@ import UserNotificationsUI //framework to customize the notification
 class ViewControllerTwo : UIViewController{
     
     @IBOutlet weak var teamSelection: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var manager: UILabel!
+    @IBOutlet weak var stadium: UILabel!
+    @IBOutlet weak var league: UILabel!
+    var currentTeam = String()
+    
+    var capacity = String()
+    
+    
+
     
     
     override func viewDidLoad() {
@@ -28,17 +39,10 @@ class ViewControllerTwo : UIViewController{
         
         
         teamSelection.text = currentTeam
-        
+        factSetup()
         
     }
     
-    var currentTeam = String()
-    var stadium = "stadium"
-    var nickname = "nickname"
-    var location = "location"
-    var manager = "manager"
-    var league = "league"
-    var capacity = "capacity"
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -58,8 +62,8 @@ class ViewControllerTwo : UIViewController{
 
     func scheduleNotification (inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()){
         let notif = UNMutableNotificationContent()
-        notif.title = "New Notification"
-        notif.subtitle = "Subtitle"
+        notif.title = "Football Facts"
+        notif.subtitle = "Here is your fact as requested!"
         notif.body = "This is a body"
         
         let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
@@ -76,7 +80,71 @@ class ViewControllerTwo : UIViewController{
             })
     }
     
-    
+    func factSetup(){
+        if ((currentTeam) == "Bournemouth"){
+            stadium.text = "    Stadium: Dean Court"
+            nickname.text = "    Nickname: The Cherries"
+            location.text = "    Location: Bournemouth, Dorset"
+            manager.text = "    Manager: Eddie Howe"
+            league.text = "    League: Premier League"
+            capacity = "11,464"
+        }
+        if ((currentTeam) == "Arsenal"){
+            stadium.text = "    Stadium: Emirates Stadium"
+            nickname.text = "    Nickname: The Gunners"
+            location.text = "    Location: Highbury, London"
+            manager.text = "    Manager: Arsene Wenger"
+            league.text = "    League: Premier League"
+            capacity = "60,432"
+        }
+        if ((currentTeam) == "Burnley"){
+            stadium.text = "    Stadium: Turf Moor"
+            nickname.text = "    Nickname: The Clarets"
+            location.text = "    Location: Burnley, Lancashire"
+            manager.text = "    Manager: Sean Dyche"
+            league.text = "    League: Premier League"
+            capacity = "21,401"
+        }
+        if ((currentTeam) == "Chelsea"){
+            stadium.text = "    Stadium:  Stamford Bridge"
+            nickname.text = "    Nickname: The Blues"
+            location.text = "    Location: Fulham, London"
+            manager.text = "    Manager: Antonio Conte"
+            league.text = "    League: Premier League"
+            capacity = "41,631"
+        }
+        if ((currentTeam) == "Crystal Palace"){
+            stadium.text = "    Stadium: Selhurst Park"
+            nickname.text = "    Nickname: The Eagles"
+            location.text = "    Location: South Norwood, London"
+            manager.text = "    Manager: Alan Pardew"
+            league.text = "    League: Premier League"
+            capacity = "25,456"
+        }
+        if ((currentTeam) == "Everton"){
+            stadium.text = "    Stadium: Goodison Park"
+            nickname.text = "    Nickname: The Toffees"
+            location.text = "    Location: Liverpool"
+            manager.text = "    Manager: Ronald Koeman"
+            league.text = "    League: Premier League"
+            capacity = "39,572"
+        }
+        if ((currentTeam) == "Hull City"){
+            stadium.text = "    Stadium: KCOM Stadium"
+            nickname.text = "    Nickname: The Tigers"
+            location.text = "    Location: Hull,Yorkshire"
+            manager.text = "    Manager: Mike Phelan"
+            league.text = "    League: Premier League"
+            capacity = "25,450"
+        }
+        if ((currentTeam) == "Leicester City"){
+            stadium.text = "    Stadium: King Power Stadium"
+            nickname.text = "    Nickname: The Foxes"
+            location.text = "    Location: Leicester"
+            manager.text = "    Manager: Claudio Ranieri"
+            league.text = "    League: Premier League"
+            capacity = "32,312"
+        }
+    }
 }
 
-    
