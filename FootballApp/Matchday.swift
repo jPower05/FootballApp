52270ec88b7a1777dd2711deec1305d319c55291
20067779 <20067779@it103imac09.wit.ie>
2016-12-08 10:36:24 +0000

    Added method to take in json

---
+++ FootballApp/Matchday.swift
@@ -0,0 +1,17 @@
+//
+//  Matchday.swift
+//  FootballApp
+//
+//  Created by 20067779 on 08/12/2016.
+//  Copyright © 2016 20067779. All rights reserved.
+//
+
+import Foundation
+
+
+public class Matchday: NSObject {
+    var matchdayName: String = ""
+    var matchDate: String = ""
+    var matchesArray = [String]()
+    
+}
--- FootballApp/ViewControllerTwo.swift
+++ FootballApp/ViewControllerTwo.swift
@@ -39,7 +39,8 @@ class ViewControllerTwo : UIViewController{
         
         
         teamSelection.text = currentTeam
-        factSetup()
+        getData()
+        
         
     }
     
@@ -75,72 +76,40 @@ class ViewControllerTwo : UIViewController{
             }
             })
     }
-
-    func factSetup(){
-        if ((currentTeam) == "Bournemouth"){
-            stadium.text = "    Stadium: Dean Court"
-            nickname.text = "    Nickname: The Cherries"
-            location.text = "    Location: Bournemouth, Dorset"
-            manager.text = "    Manager: Eddie Howe"
-            league.text = "    League: Premier League"
-            capacity = "11,464"
-        }
-        if ((currentTeam) == "Arsenal"){
-            stadium.text = "    Stadium: Emirates Stadium"
-            nickname.text = "    Nickname: The Gunners"
-            location.text = "    Location: Highbury, London"
-            manager.text = "    Manager: Arsene Wenger"
-            league.text = "    League: Premier League"
-            capacity = "60,432"
-        }
-        if ((currentTeam) == "Burnley"){
-            stadium.text = "    Stadium: Turf Moor"
-            nickname.text = "    Nickname: The Clarets"
-            location.text = "    Location: Burnley, Lancashire"
-            manager.text = "    Manager: Sean Dyche"
-            league.text = "    League: Premier League"
-            capacity = "21,401"
-        }
-        if ((currentTeam) == "Chelsea"){
-            stadium.text = "    Stadium:  Stamford Bridge"
-            nickname.text = "    Nickname: The Blues"
-            location.text = "    Location: Fulham, London"
-            manager.text = "    Manager: Antonio Conte"
-            league.text = "    League: Premier League"
-            capacity = "41,631"
-        }
-        if ((currentTeam) == "Crystal Palace"){
-            stadium.text = "    Stadium: Selhurst Park"
-            nickname.text = "    Nickname: The Eagles"
-            location.text = "    Location: South Norwood, London"
-            manager.text = "    Manager: Alan Pardew"
-            league.text = "    League: Premier League"
-            capacity = "25,456"
-        }
-        if ((currentTeam) == "Everton"){
-            stadium.text = "    Stadium: Goodison Park"
-            nickname.text = "    Nickname: The Toffees"
-            location.text = "    Location: Liverpool"
-            manager.text = "    Manager: Ronald Koeman"
-            league.text = "    League: Premier League"
-            capacity = "39,572"
-        }
-        if ((currentTeam) == "Hull City"){
-            stadium.text = "    Stadium: KCOM Stadium"
-            nickname.text = "    Nickname: The Tigers"
-            location.text = "    Location: Hull,Yorkshire"
-            manager.text = "    Manager: Mike Phelan"
-            league.text = "    League: Premier League"
-            capacity = "25,450"
-        }
-        if ((currentTeam) == "Leicester City"){
-            stadium.text = "    Stadium: King Power Stadium"
-            nickname.text = "    Nickname: The Foxes"
-            location.text = "    Location: Leicester"
-            manager.text = "    Manager: Claudio Ranieri"
-            league.text = "    League: Premier League"
-            capacity = "32,312"
-        }
+    
+    func getData(){
+        
+        let urlString = "https://raw.githubusercontent.com/opendatajson/football.json/master/2015-16/en.1.json"
+        
+        
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        
+        var session = URLSession.shared
+        
+        session.dataTask(with:request, completionHandler: {(data, response, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                do {
+                    
+                    print(data!)
+                    
+                    let parsedData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:Any]
+                    
+                    print(parsedData)
+                    
+                    
+                } catch let error as NSError {
+                    print(error)
+                }
+                
+            }
+            
+        }).resume()
+        
+        
     }
+
 }
 
