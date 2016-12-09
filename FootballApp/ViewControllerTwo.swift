b668633eeecedba6b1bad546676567617c2a0023
20067779 <20067779@it103imac09.wit.ie>
2016-12-09 12:19:36 +0000

    BASICALLY DONE NEEDS A BIT OF UI

--- FootballApp/ViewControllerTwo.swift
+++ FootballApp/ViewControllerTwo.swift
@@ -19,23 +19,29 @@ class ViewControllerTwo : UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var manager: UILabel!
     @IBOutlet weak var stadium: UILabel!
     @IBOutlet weak var league: UILabel!
-    var currentMatchday = String()
+    
     
     @IBOutlet weak var fixturesTable: UITableView!
     
     
-    var matchdayGames = [String]()
+    var currentMatchday = String()
+    
+    var tableData = [String]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (matchdayGames.count)
+        return (tableData.count)
+       
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
         
-        cell.textLabel!.text = matchdayGames[indexPath.row]
+        cell.textLabel!.text = tableData[indexPath.row]
+        print ("PLEASE")
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.font = UIFont(name:"Helvetica Neue Bold", size:16)
         print (cell.textLabel!.text)
-       
+        
         
         return (cell)
     }
@@ -56,15 +62,19 @@ class ViewControllerTwo : UIViewController, UITableViewDelegate, UITableViewData
         fixturesTable.delegate = self
         
         fixturesTable.dataSource = self
-
+        
         
         teamSelection.text = currentMatchday
         getData()
+        printDebug()
+        
+        
         
         
         
         
     }
+    
     
     
     
@@ -77,13 +87,14 @@ class ViewControllerTwo : UIViewController, UITableViewDelegate, UITableViewData
                 print("Error scheduling notification")
             }
         })
+        //printDebug()
     }
     
     func scheduleNotification (inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()){
         let notif = UNMutableNotificationContent()
         notif.title = "Football Facts"
         notif.subtitle = "Here is your fact as requested!"
-        notif.body = "This is a body"
+        notif.body = "The winner of last years title (2015-2016) was Leicester City, managed by Claudio Ranieri with a team that cost only £23 million."
         
         let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
         
@@ -101,75 +112,140 @@ class ViewControllerTwo : UIViewController, UITableViewDelegate, UITableViewData
     
     
     func getData(){
+        var matchdayGames = [String]()
+        
+        var currentMatchday = String()
         
         let urlString = "https://raw.githubusercontent.com/opendatajson/football.json/master/2015-16/en.1.json"
         
         
         let url = URL(string: urlString)
-        var request = URLRequest(url: url!)
+        let request = URLRequest(url: url!)
         
-        var session = URLSession.shared
+        let session = URLSession.shared
         
-        session.dataTask(with:request, completionHandler: {(data, response, error) in
-            if error != nil {
-                print(error!)
-            } else {
-                DispatchQueue.main.async {
-                    do {
-                        
-                        print(data!)
-                        
-                        let parsedData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:Any]
-                        
-                        var matchInfo = " Ooops something went wrong"
-                        
-                        if let rounds = parsedData["rounds"] as? [AnyObject] // posts started with array
-                        {
-                            for round in rounds
+      
+        
+        EZLoadingActivity.show("Loading Fixtures...", disableUI: true)
+
+        
+        let task = session.dataTask(with:request, completionHandler: {(data, response, error) in
+            if error == nil
+            {
+                DispatchQueue.main.async
+                    {
+                        do {
+                            
+                            print(data!)
+                            
+                            let parsedData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:Any]
+                            
+                            var matchInfo = " Ooops something went wrong"
+                            
+                            if let rounds = parsedData["rounds"] as? [AnyObject] // posts started with array
                             {
-                                let name = round["name"] as! String //specify as String
-                                
-                                if (name == (self.currentMatchday)){
-                                    print (name)
+                                for round in rounds
+                                {
+                                    let name = round["name"] as! String //specify as String
                                     
-                                    
-                                    
-                                    if let matches = round["matches"] as? [AnyObject]
+                                    if (name == (self.currentMatchday))
                                     {
-                                        for match in matches
+                                        //print (name)
+                                        
+                                        
+                                        
+                                        if let matches = round["matches"] as? [AnyObject]
                                         {
-                                            let date = match["date"] as! String
+                                            for match in matches
+                                            {
+                                                let date = match["date"] as! String
+                                                
+                                                let teams1 = match["team1"] as? NSDictionary
+                                                let team1code = teams1?["code"] as? String
+                                                
+                                                let teams2 = match["team2"] as? NSDictionary
+                                                let team2code = teams2?["code"] as? String
+                                                
+                                                matchInfo = (date + ": " + team1code! + " VS " + team2code!)
+                                                //print (matchInfo)
+                                                matchdayGames.append(matchInfo)
+                                                //print ("ADDED")
+                                                
+                                            }
                                             
-                                            let teams1 = match["team1"] as? NSDictionary
-                                            let team1code = teams1?["code"] as? String
                                             
-                                            let teams2 = match["team2"] as? NSDictionary
-                                            let team2code = teams2?["code"] as? String
-                                            
-                                            matchInfo = (date + ": " + team1code! + " VS " + team2code!)
-                                            print (matchInfo)
-                                            self.matchdayGames.append(matchInfo)
-                                            print ("ADDED")
                                             
                                         }
+                                        
                                     }
                                 }
+                                
+                                
+                                self.getArrayFromClosure(inputArray: matchdayGames)
+
                             }
+                            // Kill the CosyLoadingActivity
+                            EZLoadingActivity.hide(true, animated: false)
+                            
                         }
-                        else
-                        {
-                            print("I could not find rounds array")
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                        catch let error as NSError{
+                            print(error)
                         }
-                    } catch let error as NSError {
-                        print(error)
-                    }
+                        
                 }
-                
+             
+            }
+            else{
+                // If an error was returned from the async task ....
+                DispatchQueue.main.async{
+                    
+                    EZLoadingActivity.hide(false, animated: false)
+                }
             }
             
-        }).resume()
+            
+                    })
+        
+        
+
+        // ends the async task. If any callbacks were needed, they'd execute after this point (I think!)
+        print ("RESUMING TASK")
+        
+        task.resume()
+     
+    }
+    
+    func getArrayFromClosure(inputArray:Array<String>){
+        for input in inputArray{
+            tableData.append(input)
+        }
+        fixturesTable.reloadData()
         
     }
     
+    //USED FOR DEBUGGING ARRAY
+    
+    func printDebug(){
+        for table in tableData{
+            print("PRINTING")
+            print (table)
+        }
+        
+    }
+
+    
 }
+
+
+
+
+
+
 
